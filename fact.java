ca93b268543dc8b92563bd2dad86dbbf611f660e
Bhanureddy700 <113880826+Bhanureddy700@users.noreply.github.com>
2023-10-04 09:42:58 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Fact{
+    public static void main(String[] args) {
+    	
+        Scanner input = new Scanner(System.in);
+    	
+        System.out.print("Enter an integer1: ");
+        int number = input.nextInt();
+int fact = 1;
+        for(int i=1;i<=number;i++){
+           fact = fact*i;
+}
+ System.out.println("factorial: "+fact);
+    }
+}
