8ff70ae6deb39939017527f54cf9e94e44346bd8
Bhanureddy700 <113880826+Bhanureddy700@users.noreply.github.com>
2023-10-04 10:51:36 +0530

    Add files via upload

---
+++ max.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class max{
+    public static void main(String[] args){
+        Scanner temp = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = temp.nextInt();
+        System.out.println("Enter the m and k value:");
+        int m = temp.nextInt();
+        int k = temp.nextInt();
+        int[] array = new int[n];
+        for(int i = 0;i<n;i++){
+            array[i] = temp.nextInt();
+        }
+        int c = 0;
+        for(int i = 0;i<n-1;i++){
+            for(int j = i+1;j<n;j++){
+                if(array[i]>array[j]){
+                    c = array[i];
+                    array[i]=array[j];
+                    array[j]=c;
+                }
+            }
+        }
+        System.out.println("after sorting:");
+        for(int i = 0;i<n;i++){
+            System.out.println(array[i]);
+        }
+System.out.println("m th maximum:"+array[0+m]);
+System.out.println("n th minimum:"+array[n-k]);
+    }
+}
